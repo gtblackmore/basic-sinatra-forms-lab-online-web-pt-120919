a2d7615032ae7cbefc5937022d7f18c4f4cd0449
George Blackmore <gtblackmore@gmail.com>
2020-03-17 00:00:26 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,10 +3,10 @@ require 'sinatra/base'
 class App < Sinatra::Base
 
   get '/newteam' do
-    erb: :newteam
+    erb :newteam
   end
   
   post '/team' do
-    erb: :team
+    erb :team
   end
 end
